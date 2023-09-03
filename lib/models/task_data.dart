eecbdf2d0053f5f547bf657c5e7c68b4923d80d2
Yujin Yano <northwindapps@gmail.com>
2023-09-03 10:59:10 +0900

    first commit

---
+++ lib/models/task_data.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'dart:collection';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  int state = 0;
+
+  String _displayText = 'enter crypto or stock code.';
+
+  String get displayText => _displayText;
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    state = state + 1;
+    _displayText = 'set a lower limit value';
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
